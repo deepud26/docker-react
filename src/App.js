de507dd57eef63c6bc98d46c58933ed242ec5861
deepak allam <deepak.allam@soprasteria.com>
2020-02-14 00:06:44 +0530

    feature branch created

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.hellll oooo im changed Happy kiss day priya baby
+          i was changed to the feature branch
         </p>
         <a
           className="App-link"
